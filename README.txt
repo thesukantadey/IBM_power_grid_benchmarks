9367a165730a9e6a003d0df449b29c7fb95a2115
Sukanta Dey <sukantadey12@gmail.com>
2020-08-29 03:57:24 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+The files in this directoy are the IBM DC power grid benchmarks. They are
+described in the paper:
+	Power Grid Analysis Benchmarks
+	ASPDAC 2008
+
+The files are names IBMPG<x>.<ext>.bz2.
+
+To uncompress the files, use the Unix/Linux "bunzip2" command.
+
+The file extensions are:
+  .gif		a picture of the 2D distribution of voltage the ground grid
+  .spice	a SPICE format circuit of the power grid
+  .solution	the solution to the SPICE circuit
+
+Comments are welcome and should be sent to the author at
+	nassif@us.ibm.com
---
+++ download.sh
@@ -0,0 +1,50 @@
+ #IR drop  maps download
+ curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2.gif.bz2 -o ibmpg2.gif.bz2
+ curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3.gif.bz2 -o ibmpg3.gif.bz2
+ curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4.gif.bz2 -o ibmpg4.gif.bz2
+ curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5.gif.bz2 -o ibmpg5.gif.bz2
+ curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6.gif.bz2 -o ibmpg6.gif.bz2
+
+
+#IBM DC solutions download
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1.solution.bz2 -o ibmpg1.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2.solution.bz2 -o ibmpg2.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3.solution.bz2 -o ibmpg3.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4.solution.bz2 -o ibmpg4.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5.solution.bz2 -o ibmpg5.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6.solution.bz2 -o ibmpg6.solution.bz2
+
+
+#ibmpg circuits
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1.spice.bz2 -o ibmpg1.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1t.spice.bz2 -o ibmpg1t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2.spice.bz2 -o ibmpg2.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2t.spice.bz2 -o ibmpg2t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3.spice.bz2 -o ibmpg3.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3t.spice.bz2 -o ibmpg3t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4.spice.bz2 -o ibmpg4.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4t.spice.bz2 -o ibmpg4t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5.spice.bz2 -o ibmpg5.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5t.spice.bz2 -o ibmpg5t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6.spice.bz2 -o ibmpg6.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6t.spice.bz2 -o ibmpg6t.spice.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg7.zip -o ibmpg7.zip
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg8.zip -o ibmpg8.zip
+
+#
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1.solution.bz2 -o ibmpg1.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2.solution.bz2 -o ibmpg2.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3.solution.bz2 -o ibmpg3.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4.solution.bz2 -o ibmpg4.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5.solution.bz2 -o ibmpg5.solution.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6.solution.bz2 -o ibmpg6.solution.bz2
+
+
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1t.output.bz2 -o ibmpg1t.output.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg1t.output.old.bz2 -o ibmpg1t.output.old.bz2
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg2t.output.gz -o ibmpg2t.output.gz
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg3t.output.gz -o ibmpg3t.output.gz
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg4t.output.gz -o ibmpg4t.output.gz
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg5t.output.gz -o ibmpg5t.output.gz
+curl -k https://web.ece.ucsb.edu/~lip/PGBenchmarks/ibmpg/ibmpg6t.output.gz -o ibmpg6t.output.gz
+
